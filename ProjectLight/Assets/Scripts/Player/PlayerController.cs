9066ad8094a1a335aa52ac17cd957c4598650daa
miu <acbgzm@126.com>
2024-10-13 15:55:32 +0800

    input system

--- ProjectLight/Assets/Scripts/Player/PlayerController.cs
+++ ProjectLight/Assets/Scripts/Player/PlayerController.cs
@@ -1,20 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float m_input_x;
-    private float m_input_y;
-
     private Rigidbody2D m_rigidbody;
+
+    private PlayerInput m_player_input;
+
+    [SerializeField, ReadOnly] private Vector2 m_input_movement;
 
     [SerializeField] private float m_speed;
 
     public void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
+
+        m_player_input = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        m_player_input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_player_input.Disable();
     }
 
     public void Update()
@@ -29,15 +40,14 @@ public class PlayerController : MonoBehaviour
 
     private void ProcessInput()
     {
-        m_input_x = Input.GetAxisRaw("Horizontal");
-        m_input_y = Input.GetAxisRaw("Vertical");
+        m_input_movement = m_player_input.GP.Move.ReadValue<Vector2>();
     }
 
     private void PlayerMovement()
     {
-        if (m_input_x != 0 || m_input_y != 0)
+        if (m_input_movement.x != 0 || m_input_movement.y != 0)
         {
-            float2 movement_input = math.normalize(new float2(m_input_x, m_input_y));
+            float2 movement_input = math.normalize(new float2(m_input_movement.x, m_input_movement.y));
             m_rigidbody.MovePosition(m_rigidbody.position + (new Vector2(movement_input.x, movement_input.y)) * m_speed * Time.deltaTime);
         }
     }
