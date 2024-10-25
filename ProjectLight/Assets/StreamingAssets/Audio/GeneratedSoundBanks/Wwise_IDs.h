fa98ce6275a14ecd22f9baa79cc0e684f880fbce
Kirillua527 <1010751000@qq.com>
2024-10-26 03:22:26 +0800

    wwise，上一版有点问题，10.26

--- ProjectLight/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ ProjectLight/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,16 +13,51 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ARRIVAL = 3235884521U;
+        static const AkUniqueID PLAY_BIG_SPIDER_FOOTSTEPS = 1523723936U;
+        static const AkUniqueID PLAY_BIG_SPIDER_INCUBATION = 3738137027U;
+        static const AkUniqueID PLAY_BIG_SPIDER_SHOOTING = 3867217528U;
         static const AkUniqueID PLAY_BOMB_EXPLOSION = 4166525010U;
         static const AkUniqueID PLAY_BOMB_PLACE = 3175281290U;
+        static const AkUniqueID PLAY_BOMB_PLACE_01 = 2558917896U;
+        static const AkUniqueID PLAY_BOX_BREAK = 3331416737U;
+        static const AkUniqueID PLAY_CANDLE_BURNING = 4168788445U;
+        static const AkUniqueID PLAY_CHARACTER_ATTACKED = 106681101U;
         static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_LITTLE_SPIDER_FALLING = 3878302902U;
+        static const AkUniqueID PLAY_LITTLE_SPIDER_FOOTSTEPS = 1741564502U;
+        static const AkUniqueID PLAY_LITTLE_SPIDER_FOOTSTEPS_01 = 535372708U;
+        static const AkUniqueID PLAY_LITTLE_SPIDER_INCUBATION = 3020140973U;
+        static const AkUniqueID PLAY_OCTOPUS_TELEPORT = 1241574133U;
+        static const AkUniqueID PLAY_OCTOPUS_UNDER_THE_SEA = 2585982154U;
+        static const AkUniqueID PLAY_PLAYER_FOOTSTEPS = 98439365U;
+        static const AkUniqueID PLAY_SECOND_CANDLE = 1741580086U;
+        static const AkUniqueID PLAY_SPIDER_OF_HOLE = 4244968688U;
+        static const AkUniqueID PLAY_SUNRISE = 3517109807U;
         static const AkUniqueID PLAY_UI_BUTTON = 43599987U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BOMB_EXPLOSION
+        {
+            static const AkUniqueID GROUP = 621635301U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FOREST = 491961918U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SEA = 376462290U;
+            } // namespace STATE
+        } // namespace BOMB_EXPLOSION
+
+    } // namespace STATES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
 
     namespace BUSSES
